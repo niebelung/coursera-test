408a9859a37d445f4fdb6480faed949300dc03c8
Anton Krishtof <a.krischtoff@gmail.com>
2021-03-18 01:46:53 +0300

    add module 4 solution.fixes

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -33,7 +33,7 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function (window) {
+(function () {
   var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   // STEP 10:
@@ -58,9 +58,9 @@ WARNING!!! WARNING!!!
     // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
     // name in the loop.
     if ("j" === firstLetter.toLowerCase()) {
-      window.byeSpeaker.speak(name);
+      byeSpeaker.speak(name);
     } else {
-      window.helloSpeaker.speak(name);
+      helloSpeaker.speak(name);
     }
   }
-})(window);
+})();
